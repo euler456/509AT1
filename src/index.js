489c6bdbc0d1d3511915c3cc1e3a300407754bbc
euler456 <47268086+euler456@users.noreply.github.com>
2021-03-02 15:25:14 +1100

    Your awesome message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import "./index.css";
+import Ribs from './pic/Ribs.jpg';
+import XiaoLongBao from './pic/XiaoLongBao.jpg';
+import gruel from './pic/gruel.jpg';
 
 import {
   Route,
@@ -21,13 +24,14 @@ class Main extends React.Component {
         <h1>Freshly Login</h1>
         <ul id="header" class="row">
           <li><NavLink to="/" class="col">Home</NavLink></li>
-          <li><NavLink to="/Sign" class="col ">Sign Up</NavLink></li>
+          <li><NavLink to="/Login" class="col ">Login</NavLink></li>
           <li><NavLink to="/contact" class="col ">Contact</NavLink></li>
           <li><NavLink to="/Setting" class="col ">Setting</NavLink></li>
           <input id="search" type="text" placeholder="Search.."></input>
         </ul>
         <div id="content">
            <Route exact path="/" component={Home}/>
+           <Route exact path="/Login" component={Login}/>
            <Route path="/Sign" component={Sign}/>
            <Route path="/contact" component={Contact}/>
            <Route path="/Setting" component={Setting}/>
@@ -40,6 +44,37 @@ class Main extends React.Component {
 }
 
 class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <h4>Xiao Long Bao</h4>
+        <img src={XiaoLongBao} alt="XiaoLongBao"/>
+        <p>Xiaolongbao is a type of Chinese steamed bun (baozi) from Jiangsu province, especially associated with Wuxi and Shanghai (Shanghai was formerly a part of Jiangsu province)</p>
+        <h4 class="order">Order:</h4>
+        <input type="number" name="order" class="order1" placeholder="0"></input>
+        <h4>Ribs</h4>
+        <img src={Ribs} alt="Ribs"/>
+        <p>Ribs of pork, beef, lamb, and venison are a cut of meat. The term ribs usually refers to the less meaty part of the chops, often cooked as a slab (not cut into separate ribs).</p>
+        <h4 class="order">Order:</h4>
+        <input type="number" name="order" class="order1" placeholder="0"  ></input>
+        <h4>gruel</h4>
+        <img src={gruel} alt="gruel"/>
+        <p>Gruel is a food consisting of some type of cereal—such as ground oats, wheat, rye or rice—boiled in water or milk.</p>
+        <h4 class="order">Order:</h4>
+        <input type="number" name="order" class="order1" placeholder="0"  ></input>
+         <br/>
+      
+         <form action="https://localhost:9998/api.php?action=login" 
+          method="POST" id="loginform">
+        <button  id="fat-btn" class="btn btn-success">Order</button>
+        </form>
+        </div>
+
+    );
+  }
+}
+class Login extends React.Component {
   render() {
     return (
       <div>
@@ -94,7 +129,7 @@ class Sign extends React.Component {
                 <h4>Confirm password</h4>
             <input type="password" name="Confirm" placeholder="Confirm password"
                id="Confirm"  required></input>  <br />                      
-     <button name="subject" type="submit" id="fat-btn" class="btn btn-success" >Loggin</button>
+     <button name="subject" type="submit" id="fat-btn" class="btn btn-success" ><NavLink to="/Login" class="col ">Register</NavLink></button>
         </form>
       </div>
     );
@@ -105,7 +140,7 @@ class Setting extends React.Component {
     return (
       <div>
        
-         <h1>My profile</h1>
+         <h1>Edit My profile</h1>
         <form action="https://localhost:9998/api.php?action=login" 
           method="POST" id="loginform">
             <h4> Firstname</h4>
@@ -123,11 +158,9 @@ class Setting extends React.Component {
                 <h4> username</h4>
             <input type="text" name="username" placeholder="user name"
                id="reguser" required></input><br />
- <h4>Want to reset password?</h4>
-
-<button name="subject" type="submit" id="fat-btn" class="btn btn-success" >Save</button>   
-               
-     <button name="subject" type="submit" id="fat-btn" class="btn btn-success" ><NavLink to="/password" class="col ">password</NavLink></button>
+      <h4>Want to reset password?</h4>   
+     <button name="subject" type="submit" id="fat-btn" class="btn btn-success" ><NavLink to="/password" class="col ">password</NavLink></button><br />
+     <button name="subject" type="submit" id="fat-btn" class="btn btn-success" >Save</button>   
         </form>
       </div>
     );
